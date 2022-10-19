b20122f273f6869f1f0e1548d8b959bbc9a7628a
Gui Recordon <guilherme.recordon@gmail.com>
2022-10-19 12:51:58 -0300

    Set up react w vite

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,10 @@
+export const defaultTheme = {
+  white: "#fff",
+  black: "#000",
+
+  light100: '#E5FCC2',
+  green: '#9DE0AD',
+  blue: '#45ADA8',
+  darkblue: '#547980',
+  gray: '#594F4F',
+} as const

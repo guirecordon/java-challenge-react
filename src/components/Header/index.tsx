181b37187c15281347c26f9709049992b3dcaf94
Gui Recordon <guilherme.recordon@gmail.com>
2022-10-21 15:59:44 -0300

    Add modal structure with radix ui

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { HeaderContainer, HeaderContent } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog';
 import logoImg from '../../assets/logo.svg';
 
 export function Header() {
@@ -7,7 +8,22 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-        <button>Registrar horas</button>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <button>Registrar horas</button>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>
+                Registrar Horas Trabalhadas
+              </Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
